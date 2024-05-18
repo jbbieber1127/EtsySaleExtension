edc333d7f173495658af9a3d3465c44887394e20
jbbieber1127 <jbieber1127@gmail.com>
2024-05-17 22:51:39 -0400

    working sale creator

--- Scripts/CreateSalesScript.js
+++ Scripts/CreateSalesScript.js
@@ -27,6 +27,72 @@ function getShopId() {
   }
 }
 
+// Handle messages from the extension
 
-  console.log("x-csrf-token", getXCsrfToken());
-  console.log("shop_id", getShopId());
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.type === "getPageData") {
+      var pageData = {
+        xCsrfToken: getXCsrfToken(),
+        shopId: getShopId()
+      };
+      sendResponse(pageData);
+    }
+    else if (request.type === "createSales") {
+      handleCreateSalesRequest(request.createSalesRequest, 0);
+    }
+  }
+);
+
+function handleCreateSalesRequest(createSalesRequest, i){
+  if (i < createSalesRequest.numberOfSales) {
+    var secondsInADay = 24*60*60;
+    var startDateSSinceEpoch = createSalesRequest.startDate;
+    var saleStartDateSSinceEpoch = startDateSSinceEpoch + (i * createSalesRequest.daysPerSale)*secondsInADay;
+    var saleEndDateSSinceEpoch = saleStartDateSSinceEpoch + (createSalesRequest.daysPerSale)*secondsInADay - 1;
+    var promotionName = makeSaleName(saleStartDateSSinceEpoch) + "TO" + makeSaleName(saleEndDateSSinceEpoch);
+    createSale(createSalesRequest.shopId, createSalesRequest.xCsrfToken, promotionName, saleStartDateSSinceEpoch, saleEndDateSSinceEpoch, createSalesRequest.percentDiscount);
+  
+    setTimeout(() => {
+      handleCreateSalesRequest(createSalesRequest, i + 1);
+    }, 500);
+  }
+}
+
+function makeSaleName(secondSinceEpoch) {
+  var saleEndDate = new Date(secondSinceEpoch*1000);
+  var year = saleEndDate.getFullYear();
+  var day = saleEndDate.getDate();
+  var month = saleEndDate.getMonth() + 1;
+  return year.toString() + (month > 9 ? month.toString() : "0" + month.toString()) + (day > 9 ? day.toString() : "0" + day.toString());
+}
+
+function createSale(shop_id, xCsrfToken, promotion_name, start_date, end_date, percent_discount) {
+  var url = `https://www.etsy.com/api/v3/ajax/shop/${shop_id}/sales-coupons/create`;
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('x-csrf-token', xCsrfToken);
+
+  var salesBody = {
+    "promotion_type": "percent_discount_on_entire_order",
+    "discoverability_type": "sale",
+    "venue_id": 1, // TODO?
+    "start_date": start_date,
+    "currency_code": "USD", // TODO?
+    "promotion_name": promotion_name,
+    "end_date": end_date,
+    "reward_set_listing_ids": "",
+    "condition_min_total_order_price": null, 
+    "condition_min_num_order_items": null,
+    "reward_fixed_discount_on_order": null, 
+    "reward_percent_discount_on_order": percent_discount,
+    "seller_description": "",
+    "is_thank_you": false,
+    "eligible_region_id": null, 
+    "coupon_created_from": "seller-platform-mcnav",
+    "is_share_and_save": true
+  };
+
+  xhr.send(JSON.stringify(salesBody));
+}
--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,18 +1,125 @@
+const salesPageUrl = "https://www.etsy.com/your/shops/me/sales-discounts/step/createSale";
+
+var uaidCookie;
+var sessionKeyWwwCookie;
+var xCsrfToken;
+var shopId;
+
+/// Set up helper functions
+
 function getUaidCookie() {
-var cookieDetails = {name: "uaid", url: "https://www.etsy.com"};
-chrome.cookies.get(cookieDetails, function(cookie){
-  console.log(cookie);
-  return cookie;
-});
+  var cookieDetails = {name: "uaid", url: "https://www.etsy.com"};
+  chrome.cookies.get(cookieDetails, function(cookie){
+    uaidCookie = cookie;
+  });
 }
 
 function getSessionKeyWwwCookie() {
-var cookieDetails = {name: "session-key-www", url: "https://www.etsy.com"};
-chrome.cookies.get(cookieDetails, function(cookie){
-  console.log(cookie);
-  return cookie;
-});
+  var cookieDetails = {name: "session-key-www", url: "https://www.etsy.com"};
+  chrome.cookies.get(cookieDetails, function(cookie){
+    sessionKeyWwwCookie = cookie;
+  });
 }
 
-console.log("x-csrf-token", getUaidCookie());
-console.log("x-csrf-token", getSessionKeyWwwCookie());
+function buildAndSendCreateSalesRequest() {
+  var createSalesRequest = buildCreateSalesRequest();
+  sendCreateSalesRequest(createSalesRequest);
+}
+
+function buildCreateSalesRequest(){
+  var startDatePicker = document.getElementById("startDatePicker");
+  var startDate = startDatePicker.valueAsNumber / 1000 + (new Date()).getTimezoneOffset()*60;
+  var nbrDaysPerSale = document.getElementById("nbrDaysPerSale");
+  var daysPerSale = parseInt(nbrDaysPerSale.value);
+  var nbrSales = document.getElementById("nbrSales");
+  var numberOfSales = parseInt(nbrSales.value);
+  var pctOff = document.getElementById("pctOff");
+  var percentDiscount = parseInt(pctOff.value);
+  return {
+    shopId, xCsrfToken, percentDiscount, startDate, numberOfSales, daysPerSale
+  }
+}
+
+function parseDateISOString(s) {
+  let ds = s.split(/\D/).map(s => parseInt(s));
+  ds[1] = ds[1] - 1; // adjust month
+  return new Date(...ds);
+}
+
+function sendCreateSalesRequest(createSalesRequest){
+  (async () => {
+    const [tab] = await chrome.tabs.query({url: salesPageUrl});
+    await chrome.tabs.sendMessage(tab.id, {type: "createSales", createSalesRequest});
+  })();
+}
+
+
+function setUpExtensionView() {
+  var startDatePicker = document.getElementById("startDatePicker");
+  var divGoToSalesDashboard = document.getElementById("divGoToSalesDashboard");
+  var divCreateSales = document.getElementById("divCreateSales");
+  (async () => {
+    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    if (tab.url == salesPageUrl) {
+      divCreateSales.style["display"] = "block"
+      divGoToSalesDashboard.style["display"] = "none"
+      
+      var today = new Date(new Date().toLocaleDateString());
+      var todayDatePicker = today.toJSON().slice(0, 10);
+      startDatePicker.min = todayDatePicker;
+
+      var tomorrow = new Date(new Date().toLocaleDateString());
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      var tomorrowDatePicker = tomorrow.toJSON().slice(0, 10);
+      startDatePicker.value = tomorrowDatePicker;
+
+      sixMonthsAhead = new Date(new Date().toLocaleDateString());
+      sixMonthsAhead.setMonth(sixMonthsAhead.getMonth() + 6);
+      var sixMonthsAheadDatePicker = sixMonthsAhead.toJSON().slice(0, 10);
+      startDatePicker.max = sixMonthsAheadDatePicker;
+    }
+    else
+    {
+      divCreateSales.style["display"] = "none"
+      divGoToSalesDashboard.style["display"] = "block"
+    }
+  })();
+  
+}
+
+function handleBtnCreateSalesClick() {
+  (async () => {
+    const [tab] = await chrome.tabs.query({url: salesPageUrl});
+    const response = await chrome.tabs.sendMessage(tab.id, {type: "getPageData"});
+    // do something with response here, not outside the function
+    console.log("response", response);
+    shopId = response.shopId;
+    xCsrfToken = response.xCsrfToken;
+    buildAndSendCreateSalesRequest();
+  })();
+}
+
+/// Set up listeners for content script data
+
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log("received message", request);
+    // if (request.greeting === "hello")
+    //   sendResponse({farewell: "goodbye"});
+  }
+);
+
+/// When the page loads
+
+setUpExtensionView();
+getUaidCookie();
+getSessionKeyWwwCookie();
+
+/// Respond to form commands
+
+
+document.getElementById("btnCreateSales").onclick = () => {
+  handleBtnCreateSalesClick();
+}
